b77b1b464fc04a86427c5f50fe9dabb439a19e0b
Rishabh5506 <89128799+Rishabh5506@users.noreply.github.com>
2021-08-18 14:41:02 +0530

    created range
    shown the code

---
+++ range.py
@@ -0,0 +1,11 @@
+List1 = [12,-7,5,64,-14]
+for num in List1:
+  if num>=0:
+    print(num,end=" ")
+    
+    and
+    
+    List2 = [12,14,-95,3]
+for num in List2:
+  if num>=0:
+    print(num,end=" ")
